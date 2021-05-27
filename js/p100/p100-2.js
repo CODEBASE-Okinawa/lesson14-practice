b048d278febfc45334a725ec0358f96fbfe39793
takattty <r9r_celtics_t9t@icloud.com>
2021-05-28 00:34:39 +0900

    created answer at p100

---
+++ js/p100/p100-2.js
@@ -0,0 +1,2 @@
+const a = 2;
+console.log(a === (1 + 1));
---
+++ js/p100/p100-9.js
@@ -0,0 +1,7 @@
+for(let i=0; i<2; i++) {
+  console.log("カフェラテ");
+  for(let j=0; j<2; j++) {
+    console.log("モカ");
+  }
+}
+console.log("フラペチーノ");
